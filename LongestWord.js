b37be0f3fc185db12302e3ab186407cdef8aa82f
setisg <setisg@yahoo.com>
2014-01-20 18:53:31 -0800

    Added content

--- LongestWord.js
+++ LongestWord.js
@@ -1,1 +1,13 @@
+function LongestWord(sen) { 
 
+
+  sen = sen.replace(/[^a-z0-9\s]/gi,'').split(' ').sort(function(a,b){return b.length-a.length});
+  
+  
+  return sen; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+LongestWord("I love dogs");

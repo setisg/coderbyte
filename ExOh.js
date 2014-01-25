8bf8a738b49ae31c62f54ae9892872b28e7877dd
setisg <setisg@yahoo.com>
2014-01-25 15:44:56 -0800

    Solved with Regex

--- ExOh.js
+++ ExOh.js
@@ -1,26 +1,21 @@
 function ExOh(str) { 
 
-  str = str.split(''); 
-  x = 0;
-  o = 0;
+x = 0;
+o = 0;
   
-  for (var i = 0; i < str.length; i++){
-      if (str[i] === 'x'){
-          ++x;
-      }
-      else if (str[i] === 'o'){
-          ++o;
-      }
-      else{
-          return "You can only enter x or o";
-      }
-  }
-  
-  return (x == o);
-  
+  str = str.replace(/[xo]/gi, function(l){
+    if (l === 'x'){
+      ++x;
+    }
+    else{
+      ++o;
+    }
+  })
+  return (x === o); 
          
 }
    
 // keep this function call here 
 // to see how to enter arguments in JavaScript scroll down
 ExOh(readline());           
+          
